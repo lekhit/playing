55a261c894b5e7f933112ae00004fd180a363e86
lekhit borole <blekhit@gmail.com>
2022-11-23 11:21:31 +0530

    intial commit

---
+++ components/Question.js
@@ -0,0 +1,20 @@
+function Questions(props) {
+  return <div className="relative rounded-xl sm:p-8 p-2 md:h-[600px] md:w-[900px] h-full w-[300px] sm:h-[600px] sm:w-[400px]">
+      <div className='flex justify-center'>
+          <div className='bg-transparent flex mt-2 m-4 justify-center items-center w-[50px] h-[50px] rounded-full outline outline-4 outline-violet-900'>
+              <span className='text-white text-[30px]'>12</span>
+          </div>
+      </div>
+      <div className="mt-6">
+          <h1 className='text-white font-bold sm:text-[30px] text-[20px] text-center'>{props.question.question}</h1>
+          <div className='text-center text-[20px] text-violet-500 content-center mt-8 flex flex-wrap justify-around'>
+              <div className='bg-white p-1 md:p-2 w-[250px] md:m-4 m-3  rounded-md'>{props.question.options[0]}</div>
+              <div className='bg-white  p-1 md:p-2 w-[250px] md:m-4 m-3 rounded-md'>{props.question.options[1]}</div>
+              <div className='bg-white  p-1 md:p-2 w-[250px] md:m-4 m-3 rounded-md'>{props.question.options[2]}</div>
+              <div className='bg-white  p-1 md:p-2 w-[250px] md:m-4 m-3 rounded-md'>{props.question.options[3]}</div>
+          </div>
+      </div>
+  </div>
+}
+
+export default Questions;
---
+++ pages/question.js
@@ -0,0 +1,43 @@
+import Questions from "../components/Question";
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+let socket;
+
+function Quiz(){
+  const [input, setInput] = useState('')
+const [Question,setQuestion]=useState({number:'1',question:'What the fastest train in the India and what is its maximum speed or operating speed?',options:['Vande Bharat,130','Rajdhani Express ,120','Duranto Express,130','Bulllet Train,230']});
+  useEffect(() => {
+    socketInitializer();
+    return () => {
+      console.log("This will be logged on unmount");
+    }
+  })
+
+const socketInitializer = async () => {
+  await fetch('/api/socket');
+  socket = io()
+
+  socket.on('connect', () => {
+    console.log('connected')
+  })
+
+  socket.on('update-input', msg => {
+    setInput(msg)
+    setQuestion({...Question,question:msg});
+  })
+}
+const onChangeHandler = (e) => {
+  setInput(e.target.value)
+  socket.emit('input-change', e.target.value)
+}
+    //let Question = {number:'1',question:'What the fastest train in the India and what is its maximum speed or operating speed?',options:['Vande Bharat,130','Rajdhani Express ,120','Duranto Express,130','Bulllet Train,230']}
+    //console.log(JSON.stringify(Question));
+    return <div className="p-8 relative flex justify-center items-center w-screen h-full min-h-screen bg-gradient-to-t from-violet-800 to-violet-400">
+        
+        <div className="absolute h-screen bottom-2/3 opacity-30 right-screen sm:right-2/3 w-full sm:w-1/3 rounded-r-full bg-red-300"></div>
+        <div className="absolute h-screen bottom-2/3 opacity-30 left-screen sm:left-2/3 w-full sm:w-1/3 rounded-l-full bg-red-300"></div>
+        <Questions question={Question}/>
+    </div>
+}
+
+export default Quiz;
